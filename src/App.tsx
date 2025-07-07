49a7adbf47c4d0db6ae2d059ea08cde2578c1ccf
Alexandra Juredieu <theuleaua@gmail.com>
2025-07-07 22:34:01 +0200

    chore(install): added new dependencies for style, TailwindCSS + Shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   // This is a simple React component that renders a welcome message.
   return (
-    <>
-      <h1>Welcome to Pharmaprev</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">Welcome to Pharmaprev</h1>
       <p>This is a simple React application.</p>
-      <p>Feel free to modify it as you like!</p>
-      <p>Enjoy coding!</p>
-    </>
+      <p className="mb-4">Feel free to modify it as you like!</p>
+      <Button>Enjoy coding!</Button>
+    </div>
   )
 }
 
