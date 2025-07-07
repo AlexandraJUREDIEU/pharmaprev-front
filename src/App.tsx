486331ef1964f9aceb4cea51dcd7fb5036eceef0
Alexandra Juredieu <theuleaua@gmail.com>
2025-07-07 22:20:37 +0200

    chore(install): intialize frontend with Vite + React TS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  // This is a simple React component that renders a welcome message.
+  return (
+    <>
+      <h1>Welcome to Pharmaprev</h1>
+      <p>This is a simple React application.</p>
+      <p>Feel free to modify it as you like!</p>
+      <p>Enjoy coding!</p>
+    </>
+  )
+}
+
+export default App
